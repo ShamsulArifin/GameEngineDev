cf22f2e84a417c8689539cbd82202b249030f535
Shamsul Arifin <arifinbd6@gmail.com>
2025-04-10 11:55:24 +0600

    Added basic gameloop

---
+++ Game Loop/gameloop.cpp
@@ -0,0 +1,71 @@
+#include <SDL3/SDL.h>
+#include <iostream>
+
+int main (int argc, char* argv[])
+{
+	//Initialize SDL
+	if(SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		std::cerr << "SDL couldn't initialize! SDL_Error: " << SDL_GetError() << "\n";
+		return -1;
+	}
+
+	//Create window
+	SDL_Window *window = SDL_CreateWindow
+	(
+		"Game Loop Window",
+		800,
+		600,
+		SDL_WINDOW_RESIZABLE
+	);
+
+	if(!window)
+	{
+		std::cerr << "Window creation failed! SDL_Error: " << SDL_GetError() << "\n";
+
+		SDL_Quit();
+		return -1;
+	}
+
+	//Create renderer
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
+
+	if(!renderer)
+	{
+		std::cerr << "Renderer creation failed! SDL_Error: " << SDL_GetError() << "\n";
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return -1;
+	}
+
+	//Main game loop
+	bool isRunning = true;
+	SDL_Event event;
+
+	while (isRunning)
+	{
+		//input
+		while(SDL_PollEvent(&event))
+		{
+			if(event.type == SDL_EVENT_QUIT || (event.type == SDL_EVENT_KEY_DOWN && event.key.down == SDLK_ESCAPE))
+			{
+				isRunning = false;
+			}
+		}
+
+		//Delta time
+
+
+		//Render
+		SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255);
+		SDL_RenderClear(renderer);
+		SDL_RenderPresent(renderer);
+	}
+
+	//Cleanup
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return 0;
+}
