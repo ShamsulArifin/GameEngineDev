b4afedf0b20fef313bebb7bf190202a38c64c68a
Shamsul Arifin <arifinbd6@gmail.com>
2025-05-02 01:16:59 +0600

    Added rendering part 2 files

---
+++ Rendering Part 2/Rendering.cpp
@@ -0,0 +1,67 @@
+#include <SDL3/SDL.h>
+#include <iostream>
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+
+int main(int argc, char *argv[])
+{
+	// Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		std::cerr << "SDL couldn't initialize! SDL_Error: " << SDL_GetError() << "\n";
+		return -1;
+	}
+
+	// Create window
+	SDL_Window *window = SDL_CreateWindow(
+			"Game Loop Window",
+			SCREEN_WIDTH,
+			SCREEN_HEIGHT,
+			SDL_WINDOW_RESIZABLE);
+
+	if (!window)
+	{
+		std::cerr << "Window creation failed! SDL_Error: " << SDL_GetError() << "\n";
+
+		SDL_Quit();
+		return -1;
+	}
+
+	// Create renderer
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
+
+	if (!renderer)
+	{
+		std::cerr << "Renderer creation failed! SDL_Error: " << SDL_GetError() << "\n";
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return -1;
+	}
+
+	// Main game loop
+	bool isRunning = true;
+	SDL_Event event;
+
+	while (isRunning)
+	{
+		// input
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_EVENT_QUIT || (event.type == SDL_EVENT_KEY_DOWN && event.key.key == SDLK_ESCAPE))
+			{
+				isRunning = false;
+			}
+		}
+		// Render
+		SDL_RenderClear(renderer);
+		SDL_RenderPresent(renderer);
+	}
+
+	// Cleanup
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return 0;
+}
