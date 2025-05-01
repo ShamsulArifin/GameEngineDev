f1a093cdff3e9f4134dd18ab854a8a5f9e74d94e
Shamsul Arifin <arifinbd6@gmail.com>
2025-05-02 01:21:22 +0600

    template updated

--- SDL Template/main.cpp
+++ SDL Template/main.cpp
@@ -1,35 +1,67 @@
-#include <iostream>
 #include <SDL3/SDL.h>
+#include <iostream>
 
-const int WIDTH = 800, HEIGHT = 600;
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
 
 int main(int argc, char *argv[])
 {
-	SDL_Init(SDL_INIT_VIDEO);
-
-	SDL_Window *Window = SDL_CreateWindow("Hello world!", WIDTH, HEIGHT, SDL_WINDOW_RESIZABLE);
-
-	if (NULL == Window)
+	// Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
-		std::cout << "Could not create window: " << SDL_GetError() << std::endl;
-		return 1;
+		std::cerr << "SDL couldn't initialize! SDL_Error: " << SDL_GetError() << std::endl;
+		return -1;
 	}
 
-	SDL_Event windowEvent;
+	// Create window
+	SDL_Window *window = SDL_CreateWindow(
+			"Template",                 // change title
+			SCREEN_WIDTH,
+			SCREEN_HEIGHT,
+			SDL_WINDOW_RESIZABLE);
 
-	while (true)
+	if (!window)
 	{
-		if (SDL_PollEvent(&windowEvent))
+		std::cerr << "Window creation failed! SDL_Error: " << SDL_GetError() << std::endl;
+
+		SDL_Quit();
+		return -1;
+	}
+
+	// Create renderer
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
+
+	if (!renderer)
+	{
+		std::cerr << "Renderer creation failed! SDL_Error: " << SDL_GetError() << std::endl;
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return -1;
+	}
+
+	// Main game loop
+	bool isRunning = true;
+	SDL_Event event;
+
+	while (isRunning)
+	{
+		// input
+		while (SDL_PollEvent(&event))
 		{
-			if (SDL_EVENT_QUIT == windowEvent.type)
+			if (event.type == SDL_EVENT_QUIT || (event.type == SDL_EVENT_KEY_DOWN && event.key.key == SDLK_ESCAPE))
 			{
-				break;
+				isRunning = false;
 			}
 		}
+		// Render
+		SDL_RenderClear(renderer);
+		SDL_RenderPresent(renderer);
 	}
 
-	SDL_DestroyWindow(Window);
+	// Cleanup
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
 	SDL_Quit();
 
-	return EXIT_SUCCESS;
+	return 0;
 }
