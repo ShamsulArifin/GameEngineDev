fc356de13170fda0ee3e5fbaaea1651ef27b01de
Shamsul Arifin <arifinbd6@gmail.com>
2025-05-02 16:11:22 +0600

    geometry rotation added

--- Rendering Part 2/Rendering.cpp
+++ Rendering Part 2/Rendering.cpp
@@ -2,6 +2,10 @@
 #include <iostream>
 #include <cmath>
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 const int TARGET_FPS = 60;
@@ -41,6 +45,10 @@ int main(int argc, char *argv[])
 		SDL_Quit();
 		return -1;
 	}
+
+	SDL_Surface *surface = SDL_LoadBMP("sample.bmp");
+	SDL_Texture *boxTexture = SDL_CreateTextureFromSurface(renderer, surface);
+	SDL_DestroySurface(surface);
 
 	// * Main game loop
 	bool isRunning = true;
@@ -94,9 +102,67 @@ int main(int argc, char *argv[])
 		// * Rotating rectangle
 		SDL_FRect rotatingRect = {400, 250, 80, 80};
 		SDL_SetRenderDrawColor(renderer, 0, 120, 255, 200);
-		SDL_RenderGeometry(renderer, nullptr, nullptr, 0, nullptr, 0); // * placeholder
 
-		SDL_RenderTextureRotated(renderer, nullptr, nullptr, &rotatingRect, angle, nullptr, SDL_FLIP_NONE); // ! if using this texture
+		SDL_Vertex verts[4];
+		int indices[6] = {0, 1, 2, 2, 3, 0};
+
+		float cx = rotatingRect.x + rotatingRect.w / 2;
+		float cy = rotatingRect.y + rotatingRect.h / 2;
+
+		float x0 = rotatingRect.x;
+		float y0 = rotatingRect.y;
+
+		float x1 = rotatingRect.x + rotatingRect.w;
+		float y1 = rotatingRect.y;
+
+		float x2 = rotatingRect.x + rotatingRect.w;
+		float y2 = rotatingRect.y + rotatingRect.h;
+
+		float x3 = rotatingRect.x;
+		float y3 = rotatingRect.y + rotatingRect.h;
+
+		auto rotatePoint = [=](float &x, float &y)
+		{
+			float rad = angle * (M_PI / 180.0f);
+			float s = sin(rad);
+			float c = cos(rad);
+
+			x -= cx;
+			y -= cy;
+			float xnew = x * c - y * s;
+			float ynew = x * s + y * c;
+			x = xnew + cx;
+			y = ynew + cy;
+		};
+
+		rotatePoint(x0, y0);
+		rotatePoint(x1, y1);
+		rotatePoint(x2, y2);
+		rotatePoint(x3, y3);
+
+		SDL_Color color = {0, 120, 255, 200};
+
+		// Convert SDL_Color to SDL_FColor
+		SDL_FColor fcolor = {
+				color.r / 255.0f,
+				color.g / 255.0f,
+				color.b / 255.0f,
+				color.a / 255.0f
+			};
+
+		verts[0].position = {x0, y0};
+		verts[1].position = {x1, y1};
+		verts[2].position = {x2, y2};
+		verts[3].position = {x3, y3};
+		for (int i = 0; i < 4; i++)
+		{
+			verts[i].color = fcolor;
+			verts[i].tex_coord = {0, 0};
+		}
+
+		SDL_RenderGeometry(renderer, nullptr, verts, 4, indices, 6); // * placeholder
+
+		// SDL_RenderTextureRotated(renderer, boxTexture, nullptr, &rotatingRect, angle, nullptr, SDL_FLIP_NONE); // ! if using this texture
 
 		SDL_RenderPresent(renderer);
 
